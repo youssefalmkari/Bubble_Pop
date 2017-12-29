b66ce056f37b9609bc30c05b98ca986c811e3d65
fireyose <yose@Youssefs-MBP.home>
2017-12-29 13:13:36 -0500

    Bubble get coords function

--- _js/bubble.js
+++ _js/bubble.js
@@ -1,12 +1,19 @@
 var BubblePop = window.BubblePop || {};
 
 BubblePop.Bubble = function($) {
+
     var Bubble = function(col, row, type, sprite) {
         var that = this;
         this.getType = function() {return type;};
         this.getSprite = function() {return sprite;};
         this.getCol = function() {return col;};
         this.getRow = function() {return row;};
+        this.getCoordinates = function() {
+            var coordinates = {
+                left: that.getCol() * BubblePop.ui.BUBBLE_DIMS / 2 * BubblePop.ui.BUBBLE_DIMS / 2,
+                top: that.getRow() * BubblePop.ui.ROW_HEIGHT + BubblePop.ui.BUBBLE_DIMS / 2,
+            }
+        }
     };
 
     Bubble.create = function(rowNum, colNum, type) {
@@ -21,4 +28,5 @@ BubblePop.Bubble = function($) {
     };
 
     return Bubble;
+
 }(jQuery);
